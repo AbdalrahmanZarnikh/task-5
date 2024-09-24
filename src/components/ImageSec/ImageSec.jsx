48407af75746abc1ab089718753e46ce71b2198c
AbdAlrahman13 <blalabo89@gmail.com>
2024-09-24 15:00:33 +0300

    first commit

--- src/components/ImageSec/ImageSec.jsx
+++ src/components/ImageSec/ImageSec.jsx
@@ -3,8 +3,12 @@ import "./ImageSec.css"
 
 function ImageSec() {
   return (
-    <div className="w-100 main-margin">
-        <img src="/images/ImageSec.png" className="w-full" alt="" />
+    <div className="w-100 main-margin content-image-deal">
+        <img src="/images/ImageSec.png" className="w-full h-full" alt="" />
+        <div className="text-image-deal">
+          <h1>Find Dream Home</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing eli</p>
+        </div>
     </div>
   )
 }
